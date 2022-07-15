8c45ba0f8e00b4c31a51bad187c01596fa7cc67f
Omelian Levkovych <omelianai@gmail.com>
2022-07-15 18:09:25 +0200

    Time: 59 ms (17.48%), Space: 13.8 MB (49.73%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -1,13 +1,3 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        print(self.toLower(s))
-        return self.toLower(s)
-    
-    def toLower(self, s: str) -> str:
-        symbols = list(s)
-        
-        for i in range(len(s)):
-            symbols[i] = symbols[i].lower()
-        
-        
-        return ''.join(symbols)
+        return s.lower()
