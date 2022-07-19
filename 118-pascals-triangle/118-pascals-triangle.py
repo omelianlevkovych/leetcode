c4386acb544a350d73365693b21aae1e3d5720f5
Omelian Levkovych <omelianai@gmail.com>
2022-07-19 20:19:38 +0200

    Time: 48 ms (49.28%), Space: 13.9 MB (66.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,26 +1,8 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-    
-    #   1 [x1]
-    #  1 1 [x1, x2]
-    # 1 2 1 [x1, x2, x3]
-    # recursion?
-    
-    # [x0 = 0, xn = 0]
-    # x2 = x1 + x2, x3 = x2 + x3
-    
-        def next_level(prev):
-            new = [1] * (len(prev) + 1)
-            n = len(new)
-            
-            for i in range(1, n - 1):
-                new[i] = prev[i - 1] + prev[i]
-            return new
+        res = [[1] * i for i in range(1, numRows + 1)]
         
-        ans = [[1]]
-        cur = [1]
-        for i in range(1, numRows):
-            newRow = next_level(cur)
-            ans.append(newRow)
-            cur = newRow
-        return ans
+        for i in range(numRows):
+            for j in range(1, i):
+                res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+        return res
