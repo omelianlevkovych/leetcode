cfc7ce3f1f8e4a3daabf4b2bb2e167890a9784c6
Omelian Levkovych <omelianai@gmail.com>
2022-07-27 20:17:02 +0200

    Time: 442 ms, Memory: 16.3 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,6 +1,22 @@
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(0, len(nums)):
-            nums[i] = nums[i] * nums[i]
+    def sortedSquares(self, arr: List[int]) -> List[int]:
+        n = len(arr)
         
-        return sorted(nums)
+        ans = [0] * n
+        
+        # p - pointer, start writing from the end
+        write_p = n - 1
+        left_p = 0
+        right_p = n - 1
+        
+        while write_p >= 0:
+            if abs(arr[left_p]) > abs(arr[right_p]):
+                ans[write_p] = arr[left_p] ** 2
+                left_p += 1
+            else:
+                ans[write_p] = arr[right_p] ** 2
+                right_p -= 1
+            
+            write_p -= 1
+            
+        return ans
